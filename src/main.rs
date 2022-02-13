54596e1a5287c5bd24f17e2eb4b3dadd5a072f4e
Miles <miles@karmacheck.com>
2022-02-13 12:07:37 -0600

    a

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,31 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn solve(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
+async fn solve(request: Request, _: Context) -> Result<impl IntoResponse, Error> {
     // `serde_json::Values` impl `IntoResponse` by default
     // creating an application/json response
+    let body = request.body();
+    println!("{:?}", body);
     Ok(json!({
         "message": "Go Serverless v1.0! Your function executed successfully!"
     }))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[tokio::test]
-    async fn solve_handles() {
-        let request = Request::default();
-        let expected = json!({
-            "message": "Go Serverless v1.0! Your function executed successfully!"
-        })
-        .into_response();
-        let response = solve(request, Context::default())
-            .await
-            .expect("expected Ok(_) value")
-            .into_response();
-        assert_eq!(response.body(), expected.body())
-    }
-}
+//     #[tokio::test]
+//     async fn solve_handles() {
+//         let request = Request::default();
+//         let expected = json!({
+//             "message": "Go Serverless v1.0! Your function executed successfully!"
+//         })
+//         .into_response();
+//         let response = solve(request, Context::default())
+//             .await
+//             .expect("expected Ok(_) value")
+//             .into_response();
+//         assert_eq!(response.body(), expected.body())
+//     }
+// }
