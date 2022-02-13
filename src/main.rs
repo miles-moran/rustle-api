527844de1e5f9ac8304c73bbd0ab43aacbd4c19a
Miles <miles@karmacheck.com>
2022-02-13 11:44:44 -0600

    a

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    lambda::run(handler(hello)).await?;
+    lambda::run(handler(solve)).await?;
     Ok(())
 }
 
-async fn hello(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
+async fn solve(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
     // `serde_json::Values` impl `IntoResponse` by default
     // creating an application/json response
     Ok(json!({
@@ -22,13 +22,13 @@ mod tests {
     use super::*;
 
     #[tokio::test]
-    async fn hello_handles() {
+    async fn solve_handles() {
         let request = Request::default();
         let expected = json!({
             "message": "Go Serverless v1.0! Your function executed successfully!"
         })
         .into_response();
-        let response = hello(request, Context::default())
+        let response = solve(request, Context::default())
             .await
             .expect("expected Ok(_) value")
             .into_response();
